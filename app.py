130bac0010d9d5a99ead7dc0684c35bf154070d3
waaizulfahmi <wandaaizulf@gmail.com>
2022-11-03 08:00:25 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import pickle
+
+# filename = 'Apps_Classification.pkl'
+classifier = pickle.load(open('Apps_Classification.pkl', 'rb'))
+cv = pickle.load(open('count-Vectorizer.pkl', 'rb'))
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+    data = [message]
+    vect = cv.transform(data).toarray()
+    my_prediction = classifier.predict(vect)
+    return render_template('index.html', prediction=my_prediction)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
